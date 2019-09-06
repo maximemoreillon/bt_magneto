a2c8d94fdd16ce0383d6bd3deb3a7a672bb2e76e
Maxime Moreillon <m.moreillon@gmail.com>
2019-09-06 11:05:35 +0900

    Initial Commit

---
+++ bt_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bt_test
+//
+//  Created by Maxime Moreillon on 2019/09/06.
+//  Copyright © 2019 kitec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
